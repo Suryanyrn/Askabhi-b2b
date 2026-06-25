6ebfeeff5e482efd64e43552443ab82b578804a7
suryanyrn <suryanyrn0201@gmail.com>
2026-06-25 16:53:11 +0530

    chore: move email credentials to .env for security

--- b2b/settings.py
+++ b2b/settings.py
@@ -211,8 +211,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'askabhitechnology@gmail.com'
-EMAIL_HOST_PASSWORD = "cjtn rjva hkbs iphr"
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 LOGGING = {
     'version': 1,
